c43976f5b670675d92d3365c5d6881a5b83b04ec
Jeremy Mates <jmates@cpan.org>
2018-09-01 08:38:45 -0700

    new redraw method, update changes, str2map, and more docs

--- lib/Game/DijkstraMap.pm
+++ lib/Game/DijkstraMap.pm
@@ -15,7 +15,7 @@ use Moo;
 use namespace::clean;
 use Scalar::Util qw(looks_like_number);
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 has max_cost => ( is => 'rw', default => sub { ~0 } );
 has min_cost => ( is => 'rw', default => sub { 0 } );
@@ -31,7 +31,7 @@ has costfn   => (
         };
     },
 );
-has dimap => ( is => 'rwp', );
+has dimap => ( is => 'rw', );
 has iters => ( is => 'rwp', default => sub { 0 } );
 
 sub map {
@@ -50,7 +50,7 @@ sub map {
         }
     }
     $self->normalize_costs($dimap);
-    $self->_set_dimap($dimap);
+    $self->dimap($dimap);
     return $self;
 }
 
@@ -98,6 +98,34 @@ sub normalize_costs {
     return $self;
 }
 
+sub str2map {
+    my ( $self_or_class, $str, $lf ) = @_;
+    croak "no string given" if !defined $str;
+    $lf //= $/;
+    my @map;
+    for my $line ( split $lf, $str ) {
+        push @map, [ split //, $line ];
+    }
+    return \@map;
+}
+
+sub recalc {
+    my ($self) = @_;
+    my $dimap = $self->dimap;
+    croak "cannot recalc unset map" if !defined $dimap;
+    my $maxcost = $self->max_cost;
+    my $mincost = $self->min_cost;
+    my $maxcol  = $#{ $dimap->[0] };
+    for my $r ( 0 .. $#$dimap ) {
+        for my $c ( 0 .. $maxcol ) {
+            $dimap->[$r][$c] = $maxcost if $dimap->[$r][$c] > $mincost;
+        }
+    }
+    $self->normalize_costs($dimap);
+    $self->dimap($dimap);
+    return $self;
+}
+
 sub update {
     my $self  = shift;
     my $dimap = $self->dimap;
@@ -105,13 +133,13 @@ sub update {
     my $maxrow = $#$dimap;
     my $maxcol = $#{ $dimap->[0] };
     for my $ref (@_) {
-        croak "row $ref->[0] out of bounds" if $ref->[0] > $maxrow or $ref->[0] < 0;
-        croak "col $ref->[1] out of bounds" if $ref->[1] > $maxcol or $ref->[1] < 0;
+        my ( $r, $c ) = ( $ref->[0], $ref->[1] );
+        croak "row $r out of bounds" if $r > $maxrow or $r < 0;
+        croak "col $c out of bounds" if $c > $maxcol or $c < 0;
         croak "value must be a number" unless looks_like_number $ref->[2];
-        $dimap->[ $ref->[0] ][ $ref->[1] ] = int $ref->[2];
+        $dimap->[$r][$c] = int $ref->[2];
     }
-    $self->normalize_costs($dimap);
-    $self->_set_dimap($dimap);
+    $self->dimap($dimap);
     return $self;
 }
 
@@ -125,23 +153,41 @@ Game::DijkstraMap - a numeric grid of weights plus some related functions
 =head1 SYNOPSIS
 
   use Game::DijkstraMap;
-  my $map = Game::DijkstraMap->new;
-  $map->map( [[ ... ]] );
-  my $dimap = $map->dimap;
+  my $dm = Game::DijkstraMap->new;
 
-  $map->update( [ 0, 1, -1 ] );
+  # x is where the player is (the goal) and the rest are
+  # considered as walls or floor tiles (see the costfn)
+  my $level = Game::DijkstraMap->str2map(<<'EOM');
+  #########
+  #.h.....#
+  #.#####'#
+  #.#####x#
+  #########
+  EOM
+
+  # create the dijkstra map
+  $dm->map($level);
+
+  # change the open door ' to a closed one
+  $dm->update( [ 2, 7, -1 ] );
+  $dm->recalc;
 
 =head1 DESCRIPTION
 
-This module implements what the author of the "The Incredible Power of
-Dijkstra Maps" article (see below) calls Dijkstra Maps. Such maps have
-various uses in roguelikes or other games. This implementation may not
-be particularly fast but should allow quick prototyping of map-building
-and path-finding exercises.
+This module implements code described by "The Incredible Power of
+Dijkstra Maps" article. Such maps have various uses in roguelikes or
+other games. This implementation may not be fast but should allow quick
+prototyping of map-building and path-finding exercises.
+
+L<http://www.roguebasin.com/index.php?title=The_Incredible_Power_of_Dijkstra_Maps>
+
+The L</CONSIDERATIONS> section describes what this module does in
+more detail.
 
 =head1 CONSTRUCTOR
 
-The B<new> method accepts any of the L</ATTRIBUTES>.
+The B<new> method accepts the L</ATTRIBUTES> in the usual L<Moo>
+fashion.
 
 =head1 ATTRIBUTES
 
@@ -150,43 +196,53 @@ The B<new> method accepts any of the L</ATTRIBUTES>.
 =item B<max_cost>
 
 Cost for non-goal non-wall points. A large number by default. These
-points should be reduced by B<normalize_costs> if all goes well.
+points should be reduced to appropriate weights (steps from the nearest
+goal point) by B<normalize_costs>.
 
 =item B<min_cost>
 
-Cost for points that are goals. Zero by default.
+Cost for points that are goals (there can be multiple goals on a grid).
+Zero by default.
 
 =item B<bad_cost>
 
-Cost for cells through which motion is illegal (walls, typically). C<-1>
-by default, and ignored when updating the map.
+Cost for cells through which motion is illegal (walls, typically, though
+a map for cats may also treat water as impassable). C<-1> by default,
+and ignored when updating the map. This value for optimization purposes
+is assumed to be lower than B<min_cost>.
 
 =item B<costfn>
 
-A code reference called with the object and the contents of the map
-passed to B<map>. This function must convert those items into suitable
-cost numbers for the internal Dijkstra Map. Defaults to a function that
-assigns B<bad_cost> to C<#> and B<min_cost> to C<x>, otherwise
-B<max_cost>.
+A code reference called with the object and each cell of the I<map>
+passed to B<map>. This function must convert the contents of the cell
+into suitable cost numbers for the Dijkstra Map. Defaults to a function
+that assigns B<bad_cost> to C<#> (walls), B<min_cost> to C<x> (goals),
+and otherwise B<max_cost> for what is assumed to be floor tiles.
+
+If the I<map> is instead a grid of objects, there may need to be a
+suitable method call in those objects that returns the cost of what the
+cell contains that a custom B<costfn> then calls.
 
 =item B<dimap>
 
-Accessor for the Dijkstra Map, presently an array reference of array
-references. Do not change this reference unless you know what you
-are doing.
+The Dijkstra Map, presently an array reference of array references of
+integer values. Do not change this reference unless you know what you
+are doing. It can also be assigned to directly, for better or worse.
+
+Must not be accessed by calls to B<recalc> or B<update> before being set
+by the B<map> method (or manually).
 
 =item B<iters>
 
-This is set after B<map> or B<update> calls and indicates how many
-iterations it took the B<normalize_costs> method to stabilize the map.
+This is set after the B<map> and B<recalc> method calls and indicates
+how many iterations it took B<normalize_costs> to stabilize the map.
 
 =back
 
 =head1 METHODS
 
-These return the object so can be chained with other calls. These
-methods will throw exceptions if something goes awry (especially when
-given known bad input).
+These methods will throw exceptions if something goes awry (especially
+when given known bad input).
 
 =over 4
 
@@ -196,18 +252,119 @@ Accepts a level map (an array reference of array references, or a 2D
 grid) and uses the B<costfn> to convert the objects in that I<map> to
 the internal Dijkstra Map that is held in the B<dimap> attribute.
 
+Returns the object so can be chained with other calls.
+
 =item B<normalize_costs> I<dimap>
 
 Mostly an internal routine called by B<map> or B<update> that reduces
 B<max_cost> cells as appropriate relative to the connected
-B<min_cost> cells.
+B<min_cost> cells. Changes the B<iters> attribute.
+
+=item B<str2map> I<string> [ I<split-with> ]
+
+Utility method that converts string maps to a form suitable to be passed
+to the B<map> method. Without the optional I<split-with> argument the
+string will be split into lines using C<$/>.
+
+=item B<recalc>
+
+Resets the weights of all non-wall non-goal cells and then calls
+B<normalize_costs>. See below for a discussion of B<update> and
+B<recalc>.
+
+Returns the object so can be chained with other calls.
 
 =item B<update> I<[row, col, value]> ..
 
 Updates the given row and column with the given value for each array
-reference passed then updates the Dijkstra Map.
+reference passed. Does not recalculate the weights; see below for a
+longer discussion.
+
+Returns the object so can be chained with other calls.
 
 =back
+
+=head1 CONSIDERATIONS
+
+Given the map where C<h> represents a hound, C<@> our doomed yet somehow
+still optimistic hero, C<'> an open door, and so forth,
+
+    012345678
+  -+--------- turn 1
+  0|#########
+  1|#h......#
+  2|#.#####'#
+  3|#.#####@#
+  4|#########
+
+A Dijkstra Map with the player as the only goal would be the following
+grid of integers that outline the corridor leading to the player
+
+     0  1  2  3  4  5  6  7  8
+  -+--------------------------
+  0|-1|-1|-1|-1|-1|-1|-1|-1|-1
+  1|-1| 8| 7| 6| 5| 4| 3| 2|-1
+  2|-1| 9|-1|-1|-1|-1|-1| 1|-1
+  3|-1|10|-1|-1|-1|-1|-1| 0|-1
+  4|-1|-1|-1|-1|-1|-1|-1|-1|-1
+
+which allows the hound to move towards the player by trotting down the
+positive integers, or to flee by going the other way. This map may need
+to be updated when the player moves or changes the map; for example the
+player could close the door:
+
+  ######### turn 2
+  #..h....#
+  #.#####+#
+  #.#####@#
+  #########
+
+This change can be handled in various ways. A door may be as a wall to a
+hound, so updated to be one
+
+  $map->update( [ 2, 7, $map->bad_cost ] );
+
+results in the map
+
+     0  1  2  3  4  5  6  7  8
+  -+--------------------------
+  0|-1|-1|-1|-1|-1|-1|-1|-1|-1
+  1|-1| 8| 7| 6| 5| 4| 3| 2|-1
+  2|-1| 9|-1|-1|-1|-1|-1|-1|-1
+  3|-1|10|-1|-1|-1|-1|-1| 0|-1
+  4|-1|-1|-1|-1|-1|-1|-1|-1|-1
+
+and a hound waiting outside the door, ready to spring (or maybe it gets
+bored and wanders off, depending on the monster AI and how much patience
+our hero has). The situation could also be handled by not updating the
+map and code outside of this module handling the hound/closed door
+interaction.
+
+The B<recalc> method may be necessary where due to the closed door there
+is a new and longer path around to the player that should be followed:
+
+  #########      turn 2 (door was closed on turn 1)
+  #....h..#
+  #.#####+#########
+  #.#####@........#
+  #.#############.#
+  #...............#
+  #################
+
+  $map->update(...)           # case 1
+  $map->update(...)->recalc;  # case 2
+
+Case 1 would have the hound move to the door while case 2 would instead
+cause the hound to move around the long way. If the door after case 2 is
+opened and only an B<update> is done, the new shorter route would only
+be considered by monsters directly adjacent to the now open door (weight
+path 34, 1, 0 and also 33, 1, 0 if diagonal moves are permitted) and not
+those at 32, 31, etc. along the longer route; for those to see the
+change of the door another B<recalc> would need to be done.
+
+If you know all the cells that are floor tiles these could be passed to
+B<update> after the map changes or the player moves; this may be less
+expensive than B<recalc> as that must inspect each cell in the grid.
 
 =head1 BUGS
 
@@ -223,19 +380,16 @@ L<https://github.com/thrig/Game-DijkstraMap>
 
 =head2 Known Issues
 
-New code. In particular the B<update> method will need a different
-interface or an attribute to handle two different possibilities, see the
-commentary in the test code. Also need to add path finding (routes) and
-next cell (steps along routes) methods.
+New code. Need to add path finding (routes) and next cell (steps along
+routes) methods.
+
+B<normalize_costs> is not very good with long and mostly unconnected
+corridors; this could be improved on by considering adjacent unseen
+cells after a cell changes in addition to full map iterations.
 
 =head1 SEE ALSO
 
-The code in this module is based on the following article (as of
-August 2018):
-
-L<http://www.roguebasin.com/index.php?title=The_Incredible_Power_of_Dijkstra_Maps>
-
-There are various other graph and pathfinding modules on CPAN that may
+There are various other graph and path finding modules on CPAN that may
 be more suitable to the task at hand.
 
 =head1 AUTHOR
--- t/dimap.t
+++ t/dimap.t
@@ -9,11 +9,12 @@ my $deeply = \&eq_or_diff;
 use Game::DijkstraMap;
 my $map = Game::DijkstraMap->new;
 
+dies_ok( sub { $map->map("treasure") }, 'R.L.S. called' );
+dies_ok( sub { $map->recalc }, 'recalc not allowed before map is set' );
 dies_ok(
     sub { $map->update( [ 0, 0, 42 ] ) },
     'update not allowed before map is set'
 );
-dies_ok( sub { $map->map("treasure") }, 'R.L.S. called' );
 
 is( $map->max_cost, ~0 );
 is( $map->min_cost, 0 );
@@ -26,35 +27,39 @@ $map->map( [ [qw(. . .)], [qw(. . .)], [qw(. . x)] ] );
 $deeply->( $map->dimap, [ [qw(4 3 2)], [qw(3 2 1)], [qw(2 1 0)] ] );
 is( $map->iters, 5 );
 
-sub mapify {
-    my ($s) = @_;
-    my @map;
-    for my $l ( split $/, $s ) {
-        push @map, [ split //, $l ];
-    }
-    return \@map;
-}
-
+# TODO probably instead use is_deeply and then call this as the default
+# eq_or_diff output can be hard to read for grids, or figure out
+# something better from Test::Differences to use...
+#diag display $map->dimap;
 sub display {
     my ($map) = @_;
-    my $s = "\n";
+    my $s = $/;
     for my $r ( 0 .. $#$map ) {
         for my $c ( 0 .. $#{ $map->[0] } ) {
             $s .= $map->[$r][$c] . "\t";
         }
-        $s .= "\n";
+        $s .= $/;
     }
-    $s .= "\n";
+    $s .= $/;
     return $s;
 }
 
-my $level = mapify <<"EOM";
+my $level = $map->str2map(<<'EOM');
 ,#####,
 ##...##
 #..#.x#
 ##...##
 ,#####,
 EOM
+$deeply->(
+    $level,
+    [   [ ",", "#", "#", "#", "#", "#", "," ],
+        [ "#", "#", ".", ".", ".", "#", "#" ],
+        [ "#", ".", ".", "#", ".", "x", "#" ],
+        [ "#", "#", ".", ".", ".", "#", "#" ],
+        [ ",", "#", "#", "#", "#", "#", "," ],
+    ]
+);
 
 $deeply->(
     $map->map($level)->dimap,
@@ -67,7 +72,19 @@ $deeply->(
 );
 
 $map->update( [ 2, 1, 0 ] );
-#use Data::Dumper; diag display $map->dimap;
+
+$deeply->(
+    $map->dimap,
+    [   [ ~0, -1, -1, -1, -1, -1, ~0 ],
+        [ -1, -1, 4,  3,  2,  -1, -1 ],
+        [ -1, 0,  5,  -1, 1,  0,  -1 ],
+        [ -1, -1, 4,  3,  2,  -1, -1 ],
+        [ ~0, -1, -1, -1, -1, -1, ~0 ],
+    ]
+);
+
+$map->recalc;
+
 $deeply->(
     $map->dimap,
     [   [ ~0, -1, -1, -1, -1, -1, ~0 ],
@@ -77,16 +94,5 @@ $deeply->(
         [ ~0, -1, -1, -1, -1, -1, ~0 ],
     ]
 );
-
-$deeply->( $map->map( [ [qw(. . x)] ] )->dimap, [ [ 2, 1, 0 ] ] );
-$map->update( [ 0, 1, $map->bad_cost ] );
-# TODO should not-wall not-goal cells adjacent to updated cells be reset
-# to ->max_cost? maybe not, as the previous weights would still allow a
-# monster to track near to the original goal if the player has cast a
-# wall spell or something. however the change could be permanent in
-# which case probably should recalculate adjacent squares. 
-$deeply->( $map->dimap, [ [ 2, $map->bad_cost, 0 ] ] );
-$map->update( [ 0, 1, 42 ] );
-$deeply->( $map->dimap, [ [ 2, 1, 0 ] ] );
 
 plan tests => 14;
